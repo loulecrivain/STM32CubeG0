fa95db1a03f11bdb83aeba95abe7891bc758c9ba
Hugues Fruchet <hugues.fruchet@foss.st.com>
2024-05-19 16:17:37 +0200

    demo: uart unit test using USART2
    USART TX OK with USART2 PA2/PA15
        PA2  pin4 SO8    ------> USART2_TX
        PA15 pin8 SO8    ------> USART2_RX
    
    USART1 not available for user, only for system legacy bootloader...
    Only USART2 is available on SO8 package

--- Projects/STM32G0316-DISCO/Demonstrations/Src/main.c
+++ Projects/STM32G0316-DISCO/Demonstrations/Src/main.c
@@ -68,7 +68,7 @@ typedef struct
 
 /* Private macro -------------------------------------------------------------*/
 /* Private function prototypes -----------------------------------------------*/
-static void           SystemClockConfig(DemoMode_t DemoMode);
+static void           SystemClockConfig(void);
 static void           RtcConfig(void);
 static void           BackupDomainAccessEnable(void);
 static void           CheckBootReason(void);
@@ -89,9 +89,22 @@ static KeyPress_t     PushButtonInputScan(void);
 static DemoMode_t     GetDemoMode(void);
 static void           SetDemoMode(DemoMode_t DemoMode);
 
-static void           ErrorHandler(void);
+void           ErrorHandler(void);
 
 /* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart2;
+DMA_HandleTypeDef hdma_usart2_rx;
+DMA_HandleTypeDef hdma_usart2_tx;
+
+__IO ITStatus UartReady = RESET;
+
+/* Buffer used for transmission */
+uint8_t aTxBuffer[] = "CAFEFADE\n";
+#define TXBUFFERSIZE                      (sizeof(aTxBuffer))
+
+/* Buffer used for reception */
+#define RXBUFFERSIZE                      TXBUFFERSIZE
+uint8_t aRxBuffer[RXBUFFERSIZE];
 
 __IO uint32_t toto =0;
 static __IO uint32_t IoFalling;
@@ -117,9 +130,63 @@ static LedBlinkScheme_t LedBlinkScheme[DEMO_SUBMODE_NB] =
   {NRST_BLINKING_PERIOD/10, NRST_BLINKING_PERIOD/10, 4, GPIO_BLINKING_PERIOD}, /* DEMO_SUBMODE_GPIO_BLINK_WAKEUP */
 };
 
+void test_point(unsigned int nb)
+{
+  unsigned int i;
+
+  for (i = 0; i < nb; i++) {
+    BSP_LED_On(LED2);
+    HAL_Delay(200);
+
+    BSP_LED_Off(LED2);
+    HAL_Delay(200);
+  }
+  HAL_Delay(1000);
+}
+
 /* Exported variables --------------------------------------------------------*/
 uint16_t ButtonPin;
 RTC_HandleTypeDef RtcHandle = {0};
+
+/**
+  * @brief  Tx Transfer completed callback
+  * @param  UartHandle: UART handle.
+  * @note   This example shows a simple way to report end of DMA Tx transfer, and
+  *         you can add your own implementation.
+  * @retval None
+  */
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *UartHandle)
+{
+  /* Set transmission flag: transfer complete */
+  UartReady = SET;
+
+}
+
+/**
+  * @brief  Rx Transfer completed callback
+  * @param  UartHandle: UART handle
+  * @note   This example shows a simple way to report end of DMA Rx transfer, and
+  *         you can add your own implementation.
+  * @retval None
+  */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *UartHandle)
+{
+  /* Set transmission flag: transfer complete */
+  UartReady = SET;
+
+}
+
+/**
+  * @brief  UART error callbacks
+  * @param  UartHandle: UART handle
+  * @note   This example shows a simple way to report transfer error, and you can
+  *         add your own implementation.
+  * @retval None
+  */
+void HAL_UART_ErrorCallback(UART_HandleTypeDef *UartHandle)
+{
+  ErrorHandler();
+}
 
 /* Exported functions --------------------------------------------------------*/
 /**
@@ -144,15 +211,85 @@ int main(void)
   BSP_LED_Init(LED2);
 
   /* Get Demo mode */
-  demomode = GetDemoMode();
+//  demomode = GetDemoMode();
 
   /* Configure the system clock */
-  SystemClockConfig(demomode);
+  SystemClockConfig();
 
   /* RTC config */
-  RtcConfig();
+//  RtcConfig();
+
+  __HAL_RCC_GPIOA_CLK_ENABLE();
 
 
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Channel1_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel1_IRQn);
+  /* DMA1_Channel2_3_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel2_3_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel2_3_IRQn);
+
+
+  /* Config UART */
+
+
+
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+//  huart2.Init.BaudRate = 9600;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    ErrorHandler();
+  }
+  if (HAL_UARTEx_SetTxFifoThreshold(&huart2, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    ErrorHandler();
+  }
+  if (HAL_UARTEx_SetRxFifoThreshold(&huart2, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK)
+  {
+    ErrorHandler();
+  }
+  if (HAL_UARTEx_DisableFifoMode(&huart2) != HAL_OK)
+  {
+    ErrorHandler();
+  }
+
+#if 0
+
+  /* USARTx pins configuration -----------------------------------------------*/
+
+  GPIO_InitStruct.Pin       = USARTx_TX_PIN;
+  GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull      = GPIO_PULLUP;
+  GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_HIGH;
+//  GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Alternate = USARTx_ALTERNATE;
+  HAL_GPIO_Init(USARTx_TX_GPIO_PORT, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = USARTx_RX_PIN;
+  HAL_GPIO_Init(USARTx_RX_GPIO_PORT, &GPIO_InitStruct);
+#endif
+
+  //__HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /* Enable USART clock */
+  __HAL_RCC_USART2_CLK_ENABLE();
+
+
+#if 0
   /* HFR */
   while(1)
   {
@@ -189,6 +326,20 @@ int main(void)
 
   /* Infinite loop */
   while(1);
+
+#else
+
+  while(1) {
+
+  test_point(3);
+
+  if (HAL_UART_Transmit(&huart2, (uint8_t *)aTxBuffer, TXBUFFERSIZE, 1000) != HAL_OK)
+  {
+    ErrorHandler();
+  }
+  }
+
+#endif
 }
 
 
@@ -325,99 +476,47 @@ static void GpioDemoModeDoublePress(void)
 }
 
 /* Private functions ---------------------------------------------------------*/
-/**
-  * @brief  System Clock Configuration
-  *         The system Clock is configured as follow:
-  *         in case og NRST Mode:
-  *           System Clock source            = HSI
-  *           SYSCLK(Hz)                     = 16000000
-  *           HCLK(Hz)                       = 16000000
-  *           AHB Prescaler                  = 1
-  *           APB1 Prescaler                 = 1
-  *           Flash Latency(WS)              = 1
-  *         in case GPIO Mode:
-  *            System Clock source            = PLL (HSI)
-  *            SYSCLK(Hz)                     = 64000000
-  *            HCLK(Hz)                       = 64000000
-  *            AHB Prescaler                  = 1
-  *            APB1 Prescaler                 = 1
-  *            HSI Frequency(Hz)              = 16000000
-  *            PLLM                           = 4
-  *            PLLN                           = 64
-  *            PLLP                           = 16
-  *            PLLQ                           = 5
-  *            PLLR                           = 2
-  *            Flash Latency(WS)              = 2
-  * @param  None
-  * @retval None
-  */
-static void SystemClockConfig(DemoMode_t DemoMode)
+void SystemClockConfig(void)
 {
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  uint32_t flashlatency;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /* Set common parameters depending on subdemo mode */
-  RCC_OscInitStruct.OscillatorType      = (RCC_OSCILLATORTYPE_HSI | RCC_OSCILLATORTYPE_LSI);
+  /** Configure the main internal regulator output voltage
+  */
+  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSIDiv = RCC_HSI_DIV1;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.HSIState            = RCC_HSI_ON;
-  RCC_OscInitStruct.HSIDiv              = RCC_HSI_DIV1;
-  RCC_OscInitStruct.LSIState            = RCC_LSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV1;
+  RCC_OscInitStruct.PLL.PLLN = 8;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    ErrorHandler();
+  }
 
-  RCC_ClkInitStruct.ClockType      = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1);
-  RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
 
-  if(DemoMode == DEMO_MODE_GPIO)
-  {
-    /* Set HSI as PLL source to use it for max 64MHz frequency */
-    RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource       = RCC_PLLSOURCE_HSI;
-    RCC_OscInitStruct.PLL.PLLM            = RCC_PLLM_DIV4;
-    RCC_OscInitStruct.PLL.PLLN            = 64;
-    RCC_OscInitStruct.PLL.PLLP            = RCC_PLLP_DIV16;
-    RCC_OscInitStruct.PLL.PLLQ            = RCC_PLLQ_DIV2;
-    RCC_OscInitStruct.PLL.PLLR            = RCC_PLLR_DIV4;
-
-    /* Set PLL as SYSCLK source and Flash latency */
-    RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
-    flashlatency = FLASH_LATENCY_2;
-
-    /* Increase voltage scaling : set range 1 (default value) */
-    if(HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
-    {
-      ErrorHandler();
-    }
-  }
-  else
-  {
-    /* Set HSI as SYSCLK source and Flash latency */
-    RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_HSI;
-    flashlatency = FLASH_LATENCY_0;
-  }
-
-  /* Configure oscillator */
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct)!= HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     ErrorHandler();
-  }
-
-  /* Configure clocks */
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, flashlatency)!= HAL_OK)
-  {
-    ErrorHandler();
-  }
-
-  if(DemoMode == DEMO_MODE_NRST)
-  {
-    /* Decrease voltage scaling : set range 2 */
-    if(HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE2) != HAL_OK)
-    {
-      ErrorHandler();
-    }
   }
 }
-
 
 /**
   * @brief  Configure RTC
@@ -823,7 +922,7 @@ void HAL_RTCEx_WakeUpTimerEventCallback(RTC_HandleTypeDef * hrtc)
   * @param  None
   * @retval None
   */
-static void ErrorHandler(void)
+void ErrorHandler(void)
 {
   /* Deactivate Blinking scheme */
   LedBlinkSchemeOff();
